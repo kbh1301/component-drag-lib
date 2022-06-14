5f958e64ab369fe57f89c24909f229d3b5615c12
Kyle Hulvey <83362164+kbh1301@users.noreply.github.com>
2022-06-14 03:26:01 -0500

    Add files via upload

---
+++ components/component_box.js
@@ -0,0 +1,189 @@
+customElements.define(`c-box`, class extends HTMLElement {
+  css() {
+    return /*html*/ `
+      <style>
+        c-box {
+          position: absolute;
+          display: flex;
+          flex-direction: column;
+          width: fit-content;
+          height: fit-content;
+        }
+        c-box > * {
+          box-sizing: border-box;
+          border: 2px solid #4a006f;
+        }
+        .drag-handle {
+          width: 100%;
+          height: 30px;
+          background-color: #d9d9d9;
+          cursor: move;
+        }
+
+        .box {
+          width: 150px;
+          height: 150px;
+          background-color: #ad0dff;
+        }
+      </style>
+    `;
+  }
+
+  html() {
+    return /*html*/ `
+      <div class="drag-handle">Drag me!</div>
+      <div class="box"></div>
+    `;
+  }
+
+  js(innerElmts) {
+    // config options
+    let isGridSnapping = false;
+    let isGridDisplaying = true;
+    let isAlignSnapping = true;
+    let isCollisionSnapping = true;
+    let step = 15;
+    let magRange = 8;
+
+    const container = this.parentNode;
+    const dragHandle = this.querySelector(".drag-handle");
+    const dragContent = this;
+    const computeDivStyle = (div, style) => parseInt(window.getComputedStyle(div).getPropertyValue(style));
+
+    if(isGridSnapping || isGridDisplaying) container.style.backgroundSize = `${step}px ${step}px`;
+    const isMagSnapping = isAlignSnapping || isCollisionSnapping;
+
+    // listen for container child changes and update containerElmts accordingly
+    let containerElmts = Array.from(container.children);
+    if(isMagSnapping) {
+      const observer = new MutationObserver(() => containerElmts = Array.from(container.children));
+      observer.observe(container, {attributes:true, attributeFilter:['style'], subtree: true, childList:true});
+    }
+
+    // dragHandle handling
+    dragHandle.onmousedown = (event) => {
+      event.preventDefault();
+
+      // map all coordinates for snapping
+      const magPoints = new Map();
+      if(isMagSnapping) {
+        magPoints.set('magL', []);
+        magPoints.set('magR', []);
+        magPoints.set('magT', []);
+        magPoints.set('magB', []);
+
+        const siblingELmts = containerElmts.filter(elmt => elmt != this);
+        siblingELmts.forEach(sibling => {
+          const sibL = sibling.offsetLeft;
+          const sibR = sibL + sibling.offsetWidth;
+          const sibT = sibling.offsetTop;
+          const sibB = sibT + sibling.offsetHeight;
+
+          magPoints.get('magL').push(sibL);
+          magPoints.get('magR').push(sibR);
+          magPoints.get('magT').push(sibT);
+          magPoints.get('magB').push(sibB);
+        });
+      }
+
+      // compute container boundaries
+      const boundL = computeDivStyle(container, "padding-left");
+      const boundR = computeDivStyle(container, "width") - dragContent.offsetWidth + boundL;
+      const boundT = computeDivStyle(container, "padding-top");
+      const boundB = computeDivStyle(container, "height") - dragContent.offsetHeight + boundT;
+
+      //get initial cursor position relative to dragContent's offset
+      const initX = event.clientX - dragContent.offsetLeft; 
+      const initY = event.clientY - dragContent.offsetTop;
+
+      document.onmouseup = () => (document.onmouseup = document.onmousemove = null); //remove document events on mouseup
+      document.onmousemove = (event) => {
+        event.preventDefault();
+
+        //get current cursor position relative to initial cursor position
+        const curX = event.clientX - initX;
+        const curY = event.clientY - initY;
+
+        // calc target coordinates based on isGridSnapping
+        const snap = isGridSnapping ? (val, step) => Math.round(val / step) * step : null;
+        const valX = snap ? snap(curX, step) : curX;
+        const valY = snap ? snap(curY, step) : curY;
+        
+        const minmax = (val, min, max) => Math.min(Math.max(val, min), max); //clamp function
+
+        // set target coordinates
+        dragContent.style.left = minmax(valX, boundL, boundR) + "px";
+        dragContent.style.top = minmax(valY, boundT, boundB) + "px";
+
+        // handle mag snapping
+        if(isMagSnapping) {
+          // calc dragContent edges
+          const dragL = dragContent.offsetLeft;
+          const dragR = dragL + dragContent.offsetWidth;
+          const dragT = dragContent.offsetTop;
+          const dragB = dragT + dragContent.offsetHeight;
+
+          const handleMag = (mag, drag) => {
+            let magnetic;
+
+            if(magPoints.get(mag).some(elmt => {
+              if(drag > elmt - magRange && drag < elmt + magRange) {
+                magnetic = elmt;
+                return true;
+              }
+            })) {
+              switch(mag, drag) {
+                case 'magR', dragL:
+                case 'magL', dragL: return dragContent.style.left = magnetic + "px";
+                case 'magL', dragR:
+                case 'magR', dragR: return dragContent.style.left = magnetic - dragContent.offsetWidth + "px";
+                case 'magB', dragT:
+                case 'magT', dragT: return dragContent.style.top = magnetic + "px";
+                case 'magT', dragB:
+                case 'magB', dragB: return dragContent.style.top = magnetic - dragContent.offsetHeight + "px";
+              }
+            }
+          }
+          if(isAlignSnapping) {
+            handleMag('magL', dragL);
+            handleMag('magR', dragR);
+            handleMag('magT', dragT);
+            handleMag('magB', dragB);
+          }
+          if(isCollisionSnapping) {
+            handleMag('magL', dragR);
+            handleMag('magR', dragL);
+            handleMag('magB', dragT);
+            handleMag('magT', dragB);
+          }
+        };
+      };
+    };
+  };
+
+  constructor() {
+    self = super();
+  }
+  connectedCallback() {
+    const innerElmts = [];
+    this.childNodes.forEach((elmt) => innerElmts.push(elmt));
+
+    const htmlTemp = document.createElement("template");
+    htmlTemp.innerHTML += this.html();
+    if (htmlTemp.content.firstElementChild) {
+      const componentName = self.tagName
+        .toLowerCase()
+        .replace("component-", "");
+      const styleId = `style-component-${componentName}`;
+      if (!this.ownerDocument.querySelector(`#${styleId}`)) {
+        const cssTemp = document.createElement("template");
+        cssTemp.innerHTML += this.css();
+        cssTemp.content.querySelector("style").id = styleId;
+        this.ownerDocument.head.append(cssTemp.content);
+      }
+      this.prepend(htmlTemp.content);
+
+      this.js(innerElmts);
+    }
+  }
+});
